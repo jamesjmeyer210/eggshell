b87d6a6e65c2452b96d61b76886272d111afccf2
Meyer <784110@cognizant.com>
2019-11-04 09:10:34 -0600

    Added overall module file

---
+++ eggshell.js
@@ -0,0 +1,11 @@
+'use strict';
+
+const Assert = require('./src/assert');
+const HttpRequest = require('./src/httprequest');
+const HttpPost = require('./src/httppost');
+
+module.exports = {
+    Assert: Assert,
+    HttpRequest: HttpRequest,
+    HttpPost: HttpPost,
+};
