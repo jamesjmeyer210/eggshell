898501ed0c64d7b02860c5a4207931888ded9c2d
James Meyer <jamesjmeyer210@gmail.com>
2019-11-04 09:31:27 -0600

    Added httpget class

--- eggshell.js
+++ eggshell.js
@@ -3,9 +3,11 @@
 const Assert = require('./src/assert');
 const HttpRequest = require('./src/httprequest');
 const HttpPost = require('./src/httppost');
+const HttpGet = require('./src/httpget');
 
 module.exports = {
     Assert: Assert,
     HttpRequest: HttpRequest,
     HttpPost: HttpPost,
+    HttpGet: HttpGet,
 };
--- src/httpget.js
+++ src/httpget.js
@@ -1,10 +1,27 @@
-function HttpGet(origin, port, path, headers){
-	this.request = {
-		hostname: orgin,
-		port: port,
-		path: path,
-		method: 'GET',
-		headers: headers,
+'use strict';
+// imports
+const https = require("https");
+const {URL} = require("url");
+const HttpRequest = require('./httprequest');
+// shorthand
+const rule = console.assert;
+const T = (thing) => { return typeof(thing) };
+
+class HttpGet extends HttpRequest {
+
+	// -> HttpGet
+	constructor(origin, port, path, headers){
+		super('GET', origin, port, path, headers);
+	}
+
+	// -> HttpGet
+	static easy(url, headers) {
+		rule(T(url) == 'string' && T(headers) == 'object');
+		
+		url = new URL(url);
+		return new this(url.origin, url.port, url.href, headers, body);
 	}
 
 }
+
+module.exports = HttpGet;
